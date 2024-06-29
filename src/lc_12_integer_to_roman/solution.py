063346f67c3ffc15b9923c3f9cba329536556e86
Prashant Bharti <persian_indian@yahoo.co.in>
2024-06-29 09:48:52 +0530

    Solution for LC 12

--- src/lc_12_integer_to_roman/solution.py
+++ src/lc_12_integer_to_roman/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def intToRoman(self, num):
-        result = ''
+        result = []
         sym = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
         val = [1000, 900, 500,  400, 100,   90,  50,   40,  10,    9,   5,    4,   1]
         
         for i in range(len(sym)):
             while num >= val[i]:
                 num -= val[i]
-                result = ''.join([result, sym[i]])
+                result.append(sym[i])
                 
-        return result
+        return "".join(result)
