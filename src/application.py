03e1c1721dee6d3e799f889bd5adae86cb39b26d
Prashant Bharti <persian_indian@yahoo.co.in>
2024-07-07 12:51:03 +0530

    Solution for LC 26

--- src/application.py
+++ src/application.py
@@ -1,22 +1,11 @@
-from lc_25_reverse_nodes_in_k_group.listnode import ListNode
-from lc_25_reverse_nodes_in_k_group.solution import Solution
+from lc_26_remove_duplicates_from_sorted_array.solution import Solution
 
 def main():
-    n1 = ListNode(1)
-    n2 = ListNode(2)
-    n3 = ListNode(3)
-    n4 = ListNode(4)
-    n5 = ListNode(5)
+    nums = [1, 2, 2, 3, 3, 4, 5]
     
-    n1.next = n2
-    n2.next = n3
-    n3.next = n4
-    n4.next = n5
-
-    res = sol.reverseKGroup(n1, 6)
-    while res:
-        print(res.val)
-        res = res.next
+    res = sol.removeDuplicates(nums)
+    print(nums)
+    print(res)
 
 if __name__ == '__main__':
     sol = Solution()
---
+++ src/lc_26_remove_duplicates_from_sorted_array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+        
+        n = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[n] = nums[i]
+                n += 1
+                
+        return n
