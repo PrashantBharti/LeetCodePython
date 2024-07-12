eaa06eb61e81bbb3650f4f861e2cfe696b63f378
Prashant Bharti <persian_indian@yahoo.co.in>
2024-07-12 11:20:59 +0530

    Solution for LC 33

--- src/application.py
+++ src/application.py
@@ -1,8 +1,9 @@
-from lc_32_longest_valid_parentheses.solution import Solution
+from lc_33_search_in_rotated_sorted_array.solution import Solution
 
 def main():
-    s = "((())))()"
-    res = sol.longestValidParentheses(s)
+    nums = [3, 1]
+    target = 1
+    res = sol.search(nums, target)
     print(res)
 
 if __name__ == '__main__':
